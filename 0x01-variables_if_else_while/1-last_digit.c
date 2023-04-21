af8334595bc5a1885133a7b8b2e6110f44eee197
Oluwakemi Ajetunmobi <kemiajetunmobi1@gmail.com>
2023-04-21 15:13:27 -0700

    add random numbers to variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 /**
  *  * main - main block
- *  * Description: Print statement as regards to the last digit of the random number.
+ *  * Description: Print statement as regards to the last digit of the random number
  *  * Return: 0
  */
 int main(void)
 {
 	int n;
 	int m;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	m = n % 10;
@@ -20,9 +20,8 @@ int main(void)
 				n, m);
 	else if (m == 0)
 		printf("Last digit of %i is %i and is 0\n", n, m);
-       	else
+	else
 		printf("Last digit of %i is %i and is less than 6 and not 0\n",
 				n, m);
-	
 	return (0);
 }
