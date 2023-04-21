8a4fefba5d2069a25dcd570c4d1a90b516d0d43b
Oluwakemi Ajetunmobi <kemiajetunmobi1@gmail.com>
2023-04-21 14:35:47 -0700

    add random numbers to variable

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/*
+ * main: The main block 
+ * Description: It gets a random number and prints the number 
+ * if the number is positive, negative or zero then it should
+ * Return 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%i is postive\n', n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);
+	return (0);
+}
