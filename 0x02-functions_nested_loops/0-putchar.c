dd30f12701464675c897d25439472c8e3e329ef5
Oluwakemi Ajetunmobi <kemiajetunmobi1@gmail.com>
2023-04-25 12:52:19 -0700

    add a program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,20 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
  * main - Entry point
- * Description: prints '_putchar \n'
+ * Description: prints 'Holberton \n'
  * Return: Always 0 (success)
  */
 int main(void)
 {
-char str[] = "_putchar\n";
+char str[] = "Holberton\n";
 int i = 0;
 
 while (str[i] != '\0')
 	{
 	char c = str[i];
 
-	putchar(c);
+	_putchar(c);
 	i++;
 	}
-	
-printf("\n");
 return (0);
 }
