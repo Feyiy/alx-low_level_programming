2fb2be4b51570a65d052c2438a2c10c8b00f4f5a
Oluwakemi Ajetunmobi <kemiajetunmobi1@gmail.com>
2023-05-04 17:13:48 -0700

    add a function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,36 +1,34 @@
-#include "main.h"
-
 /**
-  * *cap_string - capitalizes all words of a string
-  * @s: is the String
-  *
-  * Return: s
-  */
-
+ * cap_string - capitalizes all words of a string
+ * @s: string
+ * Return: string
+ */
 char *cap_string(char *s)
 {
-	int i = 0;
+	int i;
 
-	while (s[i])
+	i = 0;
+	while (s[i] != '\0')
 	{
-		while (!(s[i] >= 'a' && s[i] <= 'z'))
-			i++;
-
-			if (s[i - 1] == ' ' ||
-			    s[i - 1] == '\t' ||
-			    s[i - 1] == '\n' ||
-			    s[i - 1] == ',' ||
-			    s[i - 1] == ';' ||
-			    s[i - 1] == '.' ||
-			    s[i - 1] == '!' ||
-			    s[i - 1] == '?' ||
-			    s[i - 1] == '"' ||
-			    s[i - 1] == '(' ||
-			    s[i - 1] == ')' ||
-			    s[i - 1] == '{' ||
-			    s[i - 1] == '}' ||
-			    i == 0)
-			s[i] -= 32;
+		if ((s[i - 1] == ' ' || s[i - 1] == '\n'
+		|| s[i - 1] == '\t' || s[i - 1] == ','
+		|| s[i - 1] == ';' || s[i - 1] == '!'
+		|| s[i - 1] == '?' || s[i - 1] == '"'
+		|| s[i - 1] == '(' || s[i - 1] == ')'
+		|| s[i - 1] == '{' || s[i - 1] == '}'
+		|| s[i - 1] == '.')
+		&& (s[i] >= 'a' && s[i] <= 'z'))
+		{
+			s[i] = s[i] - 32;
+		}
+		else if ((s[0] >= 97 && s[0] <= 122))
+		{
+			s[0] = s[0] - 32;
+		}
+		else
+		{
+			s[i] = s[i];
+		}
 		i++;
 	}
 	return (s);
