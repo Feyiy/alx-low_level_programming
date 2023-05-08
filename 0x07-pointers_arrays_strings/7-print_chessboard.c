1ad2263f3978924954439f94aa3efea82fae13db
Oluwakemi Ajetunmobi <kemiajetunmobi1@gmail.com>
2023-05-08 15:36:26 -0700

    add a function that prints the chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
- * print_chessboard - prints the chessboard
- * @a: pointer to chessboard
- */
+ * print_chessboard -a function that prints the chessboard
+ * @a: matrix
+*/
+
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (j = 0; j < 8; j++)
+	for (i = 0; i < 8; i++)
 	{
-		for (i = 0; i < 8; i++)
-			_putchar(a[j][i]);
-		_putchar('\n');
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(*(*(i + a) + j));
+		}
+	_putchar('\n');
 	}
 }
