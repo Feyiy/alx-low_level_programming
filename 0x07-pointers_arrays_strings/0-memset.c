9d957620ba1a339710abb76a9f7b05d6559db446
Oluwakemi Ajetunmobi <kemiajetunmobi1@gmail.com>
2023-05-08 14:54:34 -0700

    Main file created

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,26 +1,17 @@
 #include "main.h"
 
 /**
- * _memset - function fill the first @n bytes of the memory area pointed
- * to @s with the constant byte @b
- *
- * @n: bytes of the memory area pointed to by @s
- *
- * @s: with the constant byte @b
- *
- * @b: memory area pointer
- *
- * Return: a pointer to the memory area @s
+ * _memset - fills memory with a constant byte
+ * @s: memory area
+ * @b: constant byte
+ * @n: bytes of the memory area
+ * Return: pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	char *ptr = s;
 
-	while (i < n)
-	{
-	s[i] = b;
-	i++;
-	}
-
-	return (s);
+	while (n--)
+		*s++ = b;
+	return (ptr);
 }
