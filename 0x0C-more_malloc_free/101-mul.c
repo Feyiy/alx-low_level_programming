d04949ddf823188c00db5b7cccc75ea903c30276
Oluwakemi Ajetunmobi <kemiajetunmobi1@gmail.com>
2023-05-18 16:23:09 -0700

    program that multiplies two positive numbers

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+* main - this multiplies two positive numbers
+* Return: this returns void
+*/
+
+int main(void)
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	return (0);
+}
